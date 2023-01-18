085ddec822bc869276e65715ab21ddaad7c5b769
Luis Esteban <ebaldasseroni@gmail.com>
2023-01-17 22:27:48 -0300

    read a csv fila as dictionary

--- data-challenges/01-Python/02-Data-Sourcing/01-CSV/phone_book.py
+++ data-challenges/01-Python/02-Data-Sourcing/01-CSV/phone_book.py
@@ -3,4 +3,4 @@ import csv
 with open('data/phone_book.csv', mode='r') as csv_file:
     csv_reader = csv.DictReader(csv_file)
     for row in csv_reader:
-        print(row['first_name'], row['last_name'], row['phone_number'])
+        print(row['first_name'] , row['last_name'] , row['phone_number'])
