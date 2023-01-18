9d18bd36784db1a1e4fe2e224d5d241aec1f0856
Luis Esteban <ebaldasseroni@gmail.com>
2023-01-17 23:04:36 -0300

    Interacting with a API

--- data-challenges/01-Python/02-Data-Sourcing/02-API/test_api.py
+++ data-challenges/01-Python/02-Data-Sourcing/02-API/test_api.py
@@ -1,6 +1,6 @@
 import requests
 
-url = "https://weather.lewagon.com/geo/1.0/direct?q=Barcelona"
+url = "https://weather.lewagon.com/geo/1.0/direct?q=BuenosAires"
 response = requests.get(url).json()
 city = response[0]
 print(f"{city['name']}: ({city['lat']}, {city['lon']})")
