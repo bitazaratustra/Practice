9c2e17ab8b481a9626d42a0998b30d27d2dfc087
bitazaratustra <ebaldasseroni@gmail.com>
2024-02-04 22:36:02 +0100

    csv from python script

--- data-challenges/01-Python/02-Data-Sourcing/Optional-01-Olympic-Winter-Games/winter_olympic_games.py
+++ data-challenges/01-Python/02-Data-Sourcing/Optional-01-Olympic-Winter-Games/winter_olympic_games.py
@@ -8,7 +8,21 @@ MEDALS_FILEPATH = "data/winter.csv"
 
 def most_decorated_athlete_ever():
     """Returns who won the most winter olympic games medals (gold/silver/bronze) ever"""
-    pass  # YOUR CODE HERE
+    with open(MEDALS_FILEPATH, newline='') as csvfile:
+        data = csv.reader(csvfile, delimiter=',')
+        athletes = [row[4] for row in data]
+
+        winner = {}
+        for athlete in athletes:
+            if athlete in winner:
+                winner[athlete] += 1
+            else:
+                winner[athlete] = 1
+        most_decorated_athlete = max(winner, key=winner.get)
+        return most_decorated_athlete
+
+
+
 
 
 def country_with_most_gold_medals(min_year, max_year):
@@ -18,3 +32,6 @@ def country_with_most_gold_medals(min_year, max_year):
 def top_three_women_in_five_thousand_meters():
     """Returns the three women with the most 5000 meters medals(gold/silver/bronze)"""
     pass  # YOUR CODE HERE
+
+
+most_decorated_athlete_ever()
